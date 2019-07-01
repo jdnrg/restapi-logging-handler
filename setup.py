8cac0518230ae9ce20330e925e4fdb3fbb285aa5
James Michael DuPont <James.DuPont@nrg.com>
2019-07-01 17:29:48 -0400

    bump to 2.8

--- setup.py
+++ setup.py
@@ -10,21 +10,19 @@ except (IOError, ImportError):
 
 setup(
     name='restapi-logging-handler',
-    version='0.2.7',
+    version='0.2.8',
     description='A handler for the python logging module that sends logs \
         through any REST-ful API. With threading and Loggly support that \
         handles batch POSTS.',
     long_description=description,
     packages=['restapi_logging_handler'],
     install_requires=['requests-futures'],
-    author='RJ Gilligan, Ethan McCreadie, Mikey Reppy',
-    author_email='r.j.gilligan@nrg.com, '
-                 'ethan.mccreadie@nrg.com, '
-                 'mike.reppy@nrg.com',
-    url='https://github.com/EnergyPlus/restapi-logging-handler.git',
+    author='James DuPont',
+    author_email='james.dupont@nrg.com'
+    url='https://github.com/jdrng/restapi-logging-handler.git',
     download_url=(
-        'https://github.com/EnergyPlus/restapi-logging-handler.git/'
-        'tarball/0.2.7'
+        'https://github.com/jdnrg/restapi-logging-handler.git/'
+        'tarball/0.2.8'
     ),
     keywords=['rest', 'api', 'logging', 'handler', 'loggly'],
     classifiers=[],
